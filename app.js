4159dc7794a877129a3b6c40d49fc5ac51d5d049
phenolima <pedrooliveira0997@gmail.com>
2026-02-10 23:25:20 -0300

    resolvendo desafios

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert("Boas vindas ao jogo do numero secreto!");
 // let numeroSecreto = 5;
-let numeroMax = 10
+let numeroMax = 10000
 let numeroSecreto = parseInt(Math.random() * numeroMax + 1)
 console.log(numeroSecreto);
 let chute;
