90a67148e0ad68c949eda0344114cca23ca906dc
phenolima <pedrooliveira0997@gmail.com>
2026-02-11 22:08:23 -0300

    adicionando mensagen de tentativas

--- curso-2/app-curso/app.js
+++ curso-2/app-curso/app.js
@@ -1,4 +1,5 @@
-numeroSecreto = gerarNumeroAleatorio()
+numeroSecreto = 5;
+tentativa = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -11,6 +12,22 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 function verificarChute(){
     let chute = document.querySelector('input').value;
     console.log(chute == numeroSecreto );
+
+    let palavraTentativa = tentativa > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativa = `Voce acertou o numero secreto ${numeroSecreto} em ${tentativa} ${palavraTentativa}`;
+
+    if(chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Acertou!');
+        exibirTextoNaTela('p', mensagemTentativa);
+    } else {
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O numero secreto é menor');
+        } else{
+            exibirTextoNaTela('p', 'O numero secreto é maior');
+        }
+        tentativa++;
+    }
+    
 }
 
 function gerarNumeroAleatorio() {
